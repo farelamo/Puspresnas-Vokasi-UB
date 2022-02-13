fe20ad40b5e33c1188ee864b7d081270cd319581
Fariz Fahmi <farizfahmi674@gmail.com>
2022-02-13 21:04:45 +0700

    delete comment

--- app/controller/user.js
+++ app/controller/user.js
@@ -20,32 +20,7 @@ module.exports = {
     }
   },
 
-  // index: async (req, res) => {
-  //   sess = req.session;
-  //   if (sess.id_user == undefined) {
-  //     res.redirect('login');
-  //   } else {
-  //     db.query(
-  //       'SELECT * FROM `user` WHERE `id`=(?)',
-  //       [sess.id_user],
-  //       (error, profil) => {
-  //         db.query(
-  //           'SELECT * FROM `user`',
-  //           [sess.id_user],
-  //           (error, user) => {
-  //             res.render('../views/admin/index.ejs', {
-  //               profil,
-  //               user,
-  //               page: 'user'
-  //             });
-  //           }
-  //         );
-  //       }
-  //     );
-  //   }
-  // },
-
-   index: async (req, res) => {
+  index: async (req, res) => {
     sess = req.session;
     if (sess.id_user == undefined) {
       res.redirect('login');
